24b81743184c905e5857608afc4047d670f04023
Lakshangithub <lakshansiriwardhana99@gmail.com>
2022-11-29 12:05:21 +0530

    game

--- assignment/JS/SoftlaGAME/script.js
+++ assignment/JS/SoftlaGAME/script.js
@@ -16,3 +16,45 @@ function idleAnimation(){
 function idleAnimationStart(){
    idleAnimationNumber = setInterval(idleAnimation,200);
 }
+
+
+runImageNumber = 0;
+runAnimationNumber = 0;
+
+function runAnimation() { 
+
+    runImageNumber = runImageNumber + 1;
+
+    if (runImageNumber == 11) {
+        runImageNumber = 1;
+
+    }
+
+
+    boy.src = " Resources/run ("+runImageNumber +" ).png";
+   
+}
+
+function runAnimationStart(){
+
+    runAnimationNumber = setInterval(runAnimation,100);
+
+    clearInterval(idleAnimationNumber);
+
+}
+
+function keyCheck(event){
+    //alert(event.which);
+    //enter = 13
+
+    var keyCod = event. which;
+
+    if(keyCod == 13){
+        if(runAnimationNumber == 0){
+            runAnimationStart();
+
+        }
+
+    }
+
+    }
