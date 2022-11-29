6f275b3314c29340ad505000359badd4d3778cf9
Lakshangithub <lakshansiriwardhana99@gmail.com>
2022-11-29 10:24:23 +0530

    game

---
+++ assignment/JS/SoftlaGAME/script.js
@@ -0,0 +1,18 @@
+var boy = document.getElementById("boy");
+idleImageNumber = 0;
+idleAnimationNumber = 0;
+
+function idleAnimation(){
+
+    idleImageNumber = idleImageNumber + 1;
+    if (idleImageNumber == 11){
+        idleImageNumber = 1;
+    }
+
+
+    boy.src = "Resources/idle ("+idleImageNumber+").png";
+}
+
+function idleAnimationStart(){
+   idleAnimationNumber = setInterval(idleAnimation,200);
+}
